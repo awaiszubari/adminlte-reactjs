cecf922c0c3edfeb8e0b45d96e16223a6ec7294c
awais7 <awaiszubari@gmail.com>
2021-06-08 11:05:33 +0500

    added body content

--- adminlte-react-app/src/App.js
+++ adminlte-react-app/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Header from './layout/header';
 import Sidebar from './layout/sidebar';
+import Dashboard from './layout/dashboard';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="wrapper">
         <Header />
         <Sidebar />
+        <Dashboard />
       </div>
     );
   }
